74ba3bdfdd4b94bc19e58ab3288a43792b258bb1
unknown <quynhdinh704@gmail.com>
2025-05-07 15:54:41 +0700

    repository, service, controller [lecturerCourse]

---
+++ CTDT/src/main/java/com/project/CTDT/repository/LecturerCourseRepository.java
@@ -0,0 +1,8 @@
+package com.project.CTDT.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.CTDT.entity.LecturerCourse;
+
+public interface LecturerCourseRepository extends JpaRepository<LecturerCourse, Integer> {
+}
---
+++ CTDT/src/main/java/com/project/CTDT/service/LecturerCourseServiceImpl.java
@@ -0,0 +1,41 @@
+package com.project.CTDT.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.project.CTDT.entity.LecturerCourse;
+import com.project.CTDT.repository.LecturerCourseRepository;
+
+@Service
+public class LecturerCourseServiceImpl implements LecturerCourseService {
+
+    private final LecturerCourseRepository lecturerCourseRepository;
+
+    // Constructor injection
+    public LecturerCourseServiceImpl(LecturerCourseRepository lecturerCourseRepository) {
+        this.lecturerCourseRepository = lecturerCourseRepository;
+    }
+
+    @Override
+    public List<LecturerCourse> getAllLecturerCourses() {
+        return lecturerCourseRepository.findAll();
+    }
+
+    @Override
+    public LecturerCourse getLecturerCourseById(Integer id) {
+        Optional<LecturerCourse> optional = lecturerCourseRepository.findById(id);
+        return optional.orElseThrow(() -> new RuntimeException("LecturerCourse not found with id " + id));
+    }
+
+    @Override
+    public LecturerCourse saveLecturerCourse(LecturerCourse lecturerCourse) {
+        return lecturerCourseRepository.save(lecturerCourse);
+    }
+
+    @Override
+    public void deleteLecturerCourse(Integer id) {
+        lecturerCourseRepository.deleteById(id);
+    }
+}
